60a2d3f1c4d1e6672fc903c09e5f4ddda29e8c4a
9Cirno9 <67948065+9Cirno9@users.noreply.github.com>
2020-08-05 17:43:30 +0800

    特殊乘法（int类型）

---
+++ 特殊乘法2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    while(scanf("%d %d",&a,&b)!=EOF)
+    {
+        int buf1[100]={0},buf2[100]={0};
+        int size1=0,size2=0;
+        while(a){
+            buf1[size1++]=a%10;
+            a/=10;
+        }
+        while(b){
+            buf2[size2++]=b%10;
+            b/=10;
+        }
+        int ans=0;
+           for(int i=0;i<size1;i++)
+            for(int j=0;j<size2;j++)
+                ans+=buf1[i]*buf2[j];
+        printf("%d\n",ans);
+    }
+    return 0;
+} 
